587fc3dd3b05cd90603e226fa0d3f7e0fdf73dd3
yulianto <jooliantoo@gmail.com>
2024-06-28 13:53:06 +0700

    add mern auth projects

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -11,7 +11,7 @@ const projectsData = [
     description: "Using Bootstrap 5",
     image: "/images/projects/FE-Porto.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/portfolio-bootstrap5.git"
+    previewUrl: "https://github.com/yuliantocode93/portfolio-bootstrap5.git",
   },
 
   {
@@ -20,7 +20,7 @@ const projectsData = [
     description: "Using Bootstrap 5 and News API",
     image: "/images/projects/news.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/pencarian-berita-API.git"
+    previewUrl: "https://github.com/yuliantocode93/pencarian-berita-API.git",
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ const projectsData = [
     description: "Using TailwindCSS",
     image: "/images/projects/tailwindcss.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/portfolio-tailwindcss.git"
+    previewUrl: "https://github.com/yuliantocode93/portfolio-tailwindcss.git",
   },
   {
     id: 4,
@@ -36,8 +36,7 @@ const projectsData = [
     description: "Using ReactJS and Store API",
     image: "/images/projects/Store-API.png",
     tag: ["Web"],
-    previewUrl:
-      "https://github.com/yuliantocode93/simple-react-fakeStoreApi-1.git"
+    previewUrl: "https://github.com/yuliantocode93/simple-react-fakeStoreApi-1.git",
   },
   {
     id: 5,
@@ -45,8 +44,7 @@ const projectsData = [
     description: "Using ReactJS, Material UI and News API",
     image: "/images/projects/FE-API-News.png",
     tag: ["Web"],
-    previewUrl:
-      "https://github.com/yuliantocode93/portal-berita-reactjs-eduwork"
+    previewUrl: "https://github.com/yuliantocode93/portal-berita-reactjs-eduwork",
   },
   {
     id: 6,
@@ -54,7 +52,7 @@ const projectsData = [
     description: "Using ExpressJS, MongoDB, and MySQL",
     image: "/images/projects/RestfullAPI.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/nodejs-API-mongoDB-mySQL.git"
+    previewUrl: "https://github.com/yuliantocode93/nodejs-API-mongoDB-mySQL.git",
   },
   {
     id: 7,
@@ -62,7 +60,7 @@ const projectsData = [
     description: "Using ReactJS, Material UI and YouTube API",
     image: "/images/projects/youtube.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/YLCM-Media.git"
+    previewUrl: "https://github.com/yuliantocode93/YLCM-Media.git",
   },
   {
     id: 8,
@@ -70,8 +68,7 @@ const projectsData = [
     description: "Using ReactJS, Redux, ExpressJS and MongoDB",
     image: "/images/projects/MERN1.png",
     tag: ["Web"],
-    previewUrl:
-      "https://github.com/yuliantocode93/simple-fullstack-simple-mern.git"
+    previewUrl: "https://github.com/yuliantocode93/simple-fullstack-simple-mern.git",
   },
   {
     id: 9,
@@ -79,7 +76,7 @@ const projectsData = [
     description: "Using ReactJS, Redux, ExpressJS and MongoDB",
     image: "/images/projects/MERN2.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/crud-mern-properties.git"
+    previewUrl: "https://github.com/yuliantocode93/crud-mern-properties.git",
   },
   {
     id: 10,
@@ -87,8 +84,16 @@ const projectsData = [
     description: "Using NextJS and Firebase",
     image: "/images/projects/Shoes-store.png",
     tag: ["Web"],
-    previewUrl: "https://github.com/yuliantocode93/Next.js-With-Firebase"
-  }
+    previewUrl: "https://github.com/yuliantocode93/Next.js-With-Firebase",
+  },
+  {
+    id: 11,
+    title: "MERN Authentication",
+    description: "Using React, Redux, ExpressJS, MongoDB and Firebase",
+    image: "/images/projects/mern-auth.png",
+    tag: ["Web"],
+    previewUrl: "https://github.com/yuliantocode93/mern-authentication.git",
+  },
 ];
 
 const ProjectsSection = () => {
@@ -100,44 +105,23 @@ const ProjectsSection = () => {
     setTag(newTag);
   };
 
-  const filteredProjects = projectsData.filter((project) =>
-    project.tag.includes(tag)
-  );
+  const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
 
   const cardVariants = {
     initial: { y: 50, opacity: 0 },
-    animate: { y: 0, opacity: 1 }
+    animate: { y: 0, opacity: 1 },
   };
 
   return (
     <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
-      </h2>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjectTag
-          onClick={handleTagChange}
-          name="Web"
-          isSelected={tag === "Web"}
-        />
+        <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
-          <motion.li
-            key={index}
-            variants={cardVariants}
-            initial="initial"
-            animate={isInView ? "animate" : "initial"}
-            transition={{ duration: 0.3, delay: index * 0.4 }}
-          >
-            <ProjectCard
-              key={project.id}
-              title={project.title}
-              description={project.description}
-              imgUrl={project.image}
-              gitUrl={project.gitUrl}
-              previewUrl={project.previewUrl}
-            />
+          <motion.li key={index} variants={cardVariants} initial="initial" animate={isInView ? "animate" : "initial"} transition={{ duration: 0.3, delay: index * 0.4 }}>
+            <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl} />
           </motion.li>
         ))}
       </ul>
