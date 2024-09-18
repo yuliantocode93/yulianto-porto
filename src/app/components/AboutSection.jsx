55268bc96645b2c28cbf82a3f414384d1465a47a
yulianto <jooliantoo@gmail.com>
2024-09-18 11:10:13 +0700

    update code for contact

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,13 +9,19 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>React</li>
-        <li>TailwindCS </li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Next.js</li>
+        <li>ReactJS</li>
         <li>ExpressJS </li>
         <li>MongoDB </li>
+        <li>MySQL </li>
         <li>Bootstrap </li>
+        <li>TailwindCS </li>
       </ul>
-    )
+    ),
   },
   {
     title: "Education",
@@ -24,17 +30,21 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>Universitas Terbuka - Bachelor of Elementary School Education</li>
       </ul>
-    )
+    ),
   },
   {
     title: "Certifications",
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Eduwork - MERN Stack</li>
+        <li>
+          <a href="https://drive.google.com/file/d/1vRQua46A0gBMexhX28R7KaDuYVwwIpOv/view?usp=drive_link" target="_blank">
+            Eduwork - MERN Stack
+          </a>
+        </li>
       </ul>
-    )
-  }
+    ),
+  },
 ];
 
 const AboutSection = () => {
@@ -50,48 +60,28 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image
-          src="/images/about-image.png"
-          alt="computer"
-          width={500}
-          height={500}
-        />
+        <Image src="/images/about-image.png" alt="computer" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a programmer with a dedication to producing innovative and
-            efficient software solutions. With 7 month of experience in software
-            development, I have a deep understanding of the development cycle
-            and the ability to translate concepts into functional code. With my
-            background in Administration Staff I have built a strong foundation
-            in Web development.
+            I am a programmer with a dedication to producing innovative and efficient software solutions. With more experience in software development, I have a deep understanding of the development cycle and the ability to translate
+            concepts into functional code. With my background in Administration Staff I have built a strong foundation in Web development.
           </p>
           <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
+            <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
               {" "}
               Skills{" "}
             </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
+            <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
               {" "}
               Education{" "}
             </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
+            <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>
               {" "}
               Certifications{" "}
             </TabButton>
           </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
+          <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
         </div>
       </div>
     </section>
--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -4,21 +4,21 @@ import React from "react";
 const achievementsList = [
   {
     metric: "Projects",
-    value: "10",
-    postfix: "+"
+    value: "12",
+    postfix: "+",
   },
   {
     prefix: "~",
     metric: "Framework",
     value: "3",
-    postfix: "+"
+    postfix: "+",
   },
 
   {
-    metric: "Month of experience",
-    value: "6",
-    postfix: "+"
-  }
+    metric: "Year of experience",
+    value: "1",
+    postfix: "+",
+  },
 ];
 
 const AchievementsSection = () => {
@@ -27,10 +27,7 @@ const AchievementsSection = () => {
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-            >
+            <div key={index} className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0">
               <h2 className="text-white text-4xl font-bold flex flex-row">
                 {achievement.prefix}
                 {achievement.value}
