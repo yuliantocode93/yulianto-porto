46b39bb2953b69d44edf2c33ff05260843b7b8e1
yulianto <jooliantoo@gmail.com>
2024-09-27 09:03:38 +0700

    add cv

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1wDjfa58-l2vn0VLRAF0lAJJ4Z_M1n_Z-/view?usp=sharing"
+              href="https://drive.google.com/file/d/1ZVRX_tGk5yfQ1iqRWAK6ON8cqdG6R_pS/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
