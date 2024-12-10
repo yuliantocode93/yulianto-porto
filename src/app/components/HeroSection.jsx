8e14bc8aa0bab257d1ce98ac6387cdd81f6f67c8
yulianto <jooliantoo@gmail.com>
2024-12-10 08:51:18 +0700

    update code

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -20,19 +20,11 @@ const HeroSection = () => {
             <Link href="https://wa.me/625384391745" className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white">
               Hire Me
             </Link>
-            <Link
-              href="https://drive.google.com/file/d/1_ltoL7Sbf95KSwhwuKHfbU0ITTPJRgwq/view"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
+            <Link href="https://drive.google.com" className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
             </Link>
           </div>
         </motion.div>
-        {/* <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className="col-span-4 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image src="/images/yuli.png" alt="hero image" className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" width={300} height={300} />
-          </div>
-        </motion.div> */}
       </div>
     </section>
   );
